964073aa0ac00c2c982d2d08ac03ed1c11bf728d
LuizGustavoDiniz <gustavo.diniz.gon@gmail.com>
2025-08-08 19:20:39 -0300

    criacao da estrutura inicial e estilos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import './styles.css'
 
 const App = () => {
   return(
-    <div>
+    <div className="container">
       <h1>Todo List</h1>
+
+      <form className="task-form">
+        <label>Digite o nome da tarefa</label>
+        <input 
+        type="text"
+        name="task"
+        placeholder="Nome da tarefa" 
+        />
+
+        <button type="submit">
+
+        </button>
+      </form>
+
+      <div className="tasks">
+         <ul>
+          <li></li>
+         </ul>
+      </div>
+
     </div>
   )
 };
